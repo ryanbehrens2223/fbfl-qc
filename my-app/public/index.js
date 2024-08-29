85ea9d863cadbcccb31674034a31d7f95a228cfd
Ryan Behrens <ryanbehrens23@gmail.com>
2024-08-28 19:09:29 -0500

    Moving files and updating Dockerfile

--- my-app/public/index.js
+++ my-app/public/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { MantineProvider } from '@mantine/core';
 import './index.css';
-import App from './App';
+import App from '../src/App';
 // import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
